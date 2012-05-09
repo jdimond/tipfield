7397d044c8adf7e973eb579a5a295120375b1649
Jan Gerlinger <me@jangerlinger.de>
2012-05-10 00:02:56 +0200

    French localization, first try
    Ignore-this: 42453dbefd4d8b93d2e30c61399b1566
    
    darcs-hash:20120509220256-67e48-bcedcdaf9462ba3c35b6d464e3ad44cc6c9d40c2

--- src/main/scala/de/dimond/tippspiel/Languages.scala
+++ src/main/scala/de/dimond/tippspiel/Languages.scala
@@ -6,7 +6,8 @@ object Languages {
 
   val supportedLanguages = Seq(
     Lang(Locale.ENGLISH, "great_britain.png"),
-    Lang(Locale.GERMAN, "germany.png"))
+    Lang(Locale.GERMAN, "germany.png"),
+    Lang(Locale.FRENCH, "france.png"))
 
     case class Lang(locale: Locale, flag: String)
 }
